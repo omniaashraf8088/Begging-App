487fc34c3cba65f8016719baeabe25483ee9d124
omnia ashraf <omnia52481019@gmail.com>
2026-06-18 12:42:21 +0300

    feat: initialize Flutter application with new HomePage scaffold

---
+++ lib/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: SingleChildScrollView(
+        child: Padding(padding: EdgeInsets.all(16,
+
+        
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  
+              
+              
+                ],
+              ),
+            ],
+          ),
+        ),
+        ),
+      )
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
-void main()
+import 'package:begging_app/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(CH5_Begginer());        
+
+class CH5_Begginer extends StatelessWidget {
+  const CH5_Begginer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage()
+      
+      
+    );
+  }
+}
